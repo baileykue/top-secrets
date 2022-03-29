95c23ac31b79a376fd85ccd94359145847961714
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-03-29 15:37:32 -0700

    wrote controller, model, and service for signing in a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -12,11 +13,24 @@ describe('top-secrets routes', () => {
     pool.end();
   });
 
-  it('signs up a user', async () => {
+  it('signs UP a  new user', async () => {
     const res = await request(app)
       .post('/api/v1/users/')
       .send({ username: 'charlie', password: 'lovemesomeskittles' });
 
     expect(res.body).toEqual({ id: expect.any(String), username: 'charlie' });
+  });
+
+  it('signs IN an existing user', async () => {
+    const user = await UserService.create({
+      username: 'charlie',
+      password: 'lovemesomeskittles',
+    });
+
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ username: 'charlie', password: 'lovemesomeskittles' });
+
+    expect(res.body).toEqual({ message: 'You have been signed in!', user });
   });
 });
